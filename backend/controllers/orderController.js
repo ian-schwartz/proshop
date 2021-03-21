ebc114cdd472e8478a62ffe15b456c09f566ba4c
Ian Schwartz <ian.schwartz23@gmail.com>
2021-03-20 17:18:07 -0700

    show orders on profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -83,6 +83,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({ user: req.user._id })
+  res.json(orders)
 })
 
 export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
